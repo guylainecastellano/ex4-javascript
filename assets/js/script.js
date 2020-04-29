2bbc75b9a74b20611a7f9a202ca927b12b0280b4
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-04-29 17:18:15 +0200

    ex4 javascript

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+//utiliser une fonction et une boite de dialogue par une alerte (chaine de caractère)"bonjour" à l'intérieur de la fonction
+
+//récuperer les valeurs qui ont étaient ecrite à stocker dans les variables
